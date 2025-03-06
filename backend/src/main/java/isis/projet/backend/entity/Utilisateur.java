1e48d1b2de443defc473e9ef5e6d8323f022f61f
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2025-03-06 11:59:14 +0100

    updated version

--- backend/src/main/java/isis/projet/backend/entity/Utilisateur.java
+++ backend/src/main/java/isis/projet/backend/entity/Utilisateur.java
@@ -1,33 +1,32 @@
 package isis.projet.backend.entity;
 
 import jakarta.persistence.*;
-
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-// Un exemple d'entité
-// On utilise Lombok pour auto-générer getter / setter / toString...
-// cf. https://examples.javacodegeeks.com/spring-boot-with-lombok/
-@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
-@Entity // Une entité JPA
+
+@Entity // Une entité JPA 
+@Data //(Setter - Getter - ToString - NoArgsConstructor - RequiredArgsConstructor)
 public class Utilisateur {
+
     // Identifiant technique (clé primaire, auto-générée)
     @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    // Identifiant métier (code ISO)
-    @NotBlank // Validation : ne doit pas contenir uniquement des espaces
+
     @NotEmpty // Validation : ne doit pas être vide
     @NonNull // Lombok : génère un constructeur avec ce paramètre
     private String nom;
 
     @NonNull // Lombok : génère un constructeur avec ce paramètre
-    @NotBlank // Validation : ne doit pas contenir uniquement des espaces
     @NotEmpty // Validation : ne doit pas être vide
     private String prenom;
+
     private String email;
+
     private boolean estMasterAdministrateur;
+
     private Integer age;
+
     private boolean sexe;
 }
