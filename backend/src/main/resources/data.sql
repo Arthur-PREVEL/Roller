687674b25d8f70f47205e5b310284cc392173d43
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2025-03-12 15:10:55 +0100

    fichier modifie

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,11 @@
 -- Initialisation des tables
 INSERT INTO Country(id, code, name) VALUES
-    (1, 'FR', 'France'), -- Les clés sont déclarées auto-générées
-    (2, 'UK', 'United Kingdom'),
-    (3, 'US', 'United States of America');
+    (default, 'FR', 'France'), -- Les clés sont déclarées de façon auto-générées
+    (default, 'UK', 'United Kingdom'),
+    (default, 'US', 'United States of America'),
+    (default, 'AL', 'Albanie');
+
 -- Si on fixe les clés auto-générées, il faut réinitialiser le compteur
 -- Attention, cette commande n'est pas standard SQL
 -- ici la syntaxe pour H2
-ALTER TABLE Country ALTER COLUMN id RESTART WITH 4;
+-- ALTER TABLE Country ALTER COLUMN id RESTART WITH 5;
