ac2169d4d8d7dd520231b95a9c8a8336f6bf1ad6
NeirouzAttia <neirouz.attia@etud.univ-jfv.fr>
2025-03-13 09:50:05 +0100

    modifie

---
+++ frontend/src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import StatisticsView from "@/views/home/Statistics.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+</script>
+
+<template>
+  <div>
+    <StatisticsView />
+    <StatisticsView />
+  </div>
+   <FooterComponent />
+  <div>
+
+  </div>
+</template>
